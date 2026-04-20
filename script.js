d45accffdb86d9c1a49c2ecdc382b015c6001b4d
Khushwant kumar <khushwantkumr123@gmail.com>
2026-04-21 00:25:43 +0530

    Add dark mode toggle and contact alert function

---
+++ script.js
@@ -0,0 +1,9 @@
+// Dark Mode Toggle
+document.getElementById("toggleMode").onclick = () => {
+  document.body.classList.toggle("light");
+};
+
+// Contact Alert
+function sendMsg() {
+  alert("Message Sent!");
+}
